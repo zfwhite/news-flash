60484279442837b77bfefbf681822b684ec2bac4
Zachary White <zfwhite91@gmail.com>
2016-08-03 10:08:32 -0700

    User can search for term with a positive or negative bias. #2

--- app.js
+++ app.js
@@ -12,32 +12,61 @@ var alchemy_data_news = watson.alchemy_data_news({
   api_key: '3333158ad0e2ef353a7944bcf42b47016a390b7b'
 });
 
-app.post('/search', function(req, res) {
+app.post('/search/positive', function(req, res) {
   if (req.body.send === '') {
     res.send('blank');
   }
-  // var rawSearch = req.body.send.split(' ').join('^');
-  // var optimizedSearch = 'A[' + rawSearch + ']';
-  // console.log(optimizedSearch);
-  // var params = {
-  //   start: 'now-1d',
-  //   end: 'now',
-  //   count: 5,
-  //   return: ['enriched.url.url,enriched.url.title,enriched.url.text'],
-  //   'q.enriched.url.enrichedTitle.keywords.keyword.text': optimizedSearch
-  // }
-  //
-  // alchemy_data_news.getNews(params, function (err, news) {
-  // if (err) {
-  //   console.log('error:', err);
-  //   res.sendFile(__dirname + '/mockData.json');
-  // }
-  // else {
-  //   console.log(JSON.stringify(news, null, 2));
-  //   res.send(news);
-  // }
-  // });
-  res.sendFile(__dirname + '/mockData.json');
+  var rawSearch = req.body.send.split(' ').join('^');
+  var optimizedSearch = 'A[' + rawSearch + ']';
+  console.log(optimizedSearch);
+  var params = {
+    start: 'now-7d',
+    end: 'now',
+    count: 5,
+    return: ['enriched.url.url,enriched.url.title,enriched.url.text'],
+    'q.enriched.url.enrichedTitle.docSentiment': '|type=positive,score=>.7|',
+    'q.enriched.url.enrichedTitle.keywords.keyword.text': optimizedSearch
+  }
+
+  alchemy_data_news.getNews(params, function (err, news) {
+  if (err) {
+    console.log('error:', err);
+    res.sendFile(__dirname + '/mockData.json');
+  }
+  else {
+    console.log(JSON.stringify(news, null, 2));
+    res.send(news);
+  }
+  });
+  // res.sendFile(__dirname + '/mockData.json');
 });
 
+app.post('/search/negative', function(req, res) {
+  if (req.body.send === '') {
+    res.send('blank');
+  }
+  var rawSearch = req.body.send.split(' ').join('^');
+  var optimizedSearch = 'A[' + rawSearch + ']';
+  console.log(optimizedSearch);
+  var params = {
+    start: 'now-7d',
+    end: 'now',
+    count: 5,
+    return: ['enriched.url.url,enriched.url.title,enriched.url.text'],
+    'q.enriched.url.enrichedTitle.docSentiment': '|type=positive,score=<.3|',
+    'q.enriched.url.title': optimizedSearch
+  }
+
+  alchemy_data_news.getNews(params, function (err, news) {
+  if (err) {
+    console.log('error:', err);
+    res.sendFile(__dirname + '/mockData.json');
+  }
+  else {
+    console.log(JSON.stringify(news, null, 2));
+    res.send(news);
+  }
+  });
+  // res.sendFile(__dirname + '/mockData.json');
+});
 app.listen(3000);
